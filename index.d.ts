4ac8423cd88934a123b504eb9e6c98079caa066b
afeiship <1290657123@qq.com>
2022-09-25 11:43:19 +0800

    fix: typing with new

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,14 @@ type Destroyable = {
   destroy(): void;
 }
 
+type Options = {
+  interval: number;
+  immediate: boolean
+}
+
 interface NxStatic {
   UrlWatcher: {
+    new(options: Options)
     watch(callback: any): Destroyable;
   }
 }
